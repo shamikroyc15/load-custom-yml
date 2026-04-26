c2904e01bd0ca47e5ae10c60b5261baf46ba4fb5
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 07:58:14 -0400

    Merged Local + Stashed changes

--- src/main/java/com/yaml/load/YmlSampleTest1.java
+++ src/main/java/com/yaml/load/YmlSampleTest1.java
@@ -3,6 +3,12 @@ package com.yaml.load;
 public class YmlSampleTest1 {
 
     public void display() {
+
+        System.out.println("This is Feature 2 change - stash ");
+        System.out.println("This is Feature 2 change - stash ");
+        System.out.println("This is Feature 2 change - stash ");
+        System.out.println("This is Feature 2 change - stash ");
+
         System.out.println("This is Feature 2 change -  ");
         System.out.println("This is Feature 2 change -  ");
         System.out.println("This is Feature 2 change -  ");
