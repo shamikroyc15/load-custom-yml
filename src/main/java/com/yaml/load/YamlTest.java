1ab08009adeed5c9465cbe885488e04e3d5e3500
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 06:52:12 -0400

    Update YamlTest.java - remote

--- src/main/java/com/yaml/load/YamlTest.java
+++ src/main/java/com/yaml/load/YamlTest.java
@@ -1,2 +1,8 @@
-package com.yaml.load;public class YamlTest {
+package com.yaml.load;
+
+public class YamlTest {
+
+  public static void main(String[] args) {
+    // Change by another person
+  }
 }
