9862f89e574614c7f31b43d6a98552cd94f6c1cb
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 07:12:40 -0400

    Update Sample.java - Line 7 - remote change

--- src/main/java/com/yaml/load/Sample.java
+++ src/main/java/com/yaml/load/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("hi all");
         System.out.println("hi, how are you all ?");
+        System.out.println("Shamik - Remote Change");
     }
 }
