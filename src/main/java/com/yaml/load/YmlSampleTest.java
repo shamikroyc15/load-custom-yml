417d1c9c012181bb129d56e1dc919564edaba8f2
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 07:48:08 -0400

    Update YmlSampleTest.java - Update line 8-9 - remote

--- src/main/java/com/yaml/load/YmlSampleTest.java
+++ src/main/java/com/yaml/load/YmlSampleTest.java
@@ -4,6 +4,9 @@ public class YmlSampleTest {
 
     public void print() {
         System.out.println("This is Feature 1 change");
+
+        System.out.println("This is Feature 2 change - remote ");
+        System.out.println("This is Feature 2 change - remote ");
     }
 
 }
