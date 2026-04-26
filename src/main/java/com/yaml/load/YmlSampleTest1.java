467751b0cf0a6c3ce06e46200de3b3a0aa816471
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 07:54:48 -0400

    YampSampleTest1.java - Line 5 - 9 - Feature 2 Local Change

--- src/main/java/com/yaml/load/YmlSampleTest1.java
+++ src/main/java/com/yaml/load/YmlSampleTest1.java
@@ -1,4 +1,11 @@
 package com.yaml.load;
 
 public class YmlSampleTest1 {
+
+    public void display() {
+        System.out.println("This is Feature 2 change -  ");
+        System.out.println("This is Feature 2 change -  ");
+        System.out.println("This is Feature 2 change -  ");
+        System.out.println("This is Feature 2 change - ");
+    }
 }
