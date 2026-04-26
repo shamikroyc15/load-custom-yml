2e2abfa60ae78400d6f8769bac35be4a926adc91
shamikroyc15 <shamikroyc15@gmail.com>
2026-04-26 07:14:15 -0400

    Apply Remote change to local change

--- src/main/java/com/yaml/load/Sample.java
+++ src/main/java/com/yaml/load/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("hi all");
         System.out.println("hi, how are you all ?");
+        System.out.println("Shamik - Local Change");
         System.out.println("Shamik - Remote Change");
     }
 }
